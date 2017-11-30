73b4da101f6fa7edb51203b5205f6d32bcaa6742
HyeonseongJeon <hyeonz0415@postech.ac.kr>
2017-11-30 22:32:54 +0900

    [implementation] modify some errors

---
+++ scripts/screen.js
@@ -0,0 +1,26 @@
+function buttonStartgame()
+{
+  var x = document.getElementsByClassName("loadingScreen");
+      for (var i = 0; i < x.length; i++)
+        {
+          x[i].style.display = "block";
+        }
+}
+function buttonLoadgame()
+{
+  //document.getElementById('a').style.display='block';
+  var x = document.getElementsByClassName("loadingScreen2");
+      for (var i = 0; i < x.length; i++)
+        {
+          x[i].style.display = "block";
+        }
+}
+function buttonAchievement()
+{
+  //document.getElementById('a').style.display='block';
+  var x = document.getElementsByClassName("achievementScreen");
+      for (var i = 0; i < x.length; i++)
+        {
+          x[i].style.display = "block";
+        }
+}
