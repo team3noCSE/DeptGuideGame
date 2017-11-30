e7dc9f9483e58b52e95186f32f59c4f17d957913
HyeonseongJeon <hyeonz0415@postech.ac.kr>
2017-11-30 22:47:04 +0900

    [implementation] hide initial menu onclick menu buttons

--- scripts/screen.js
+++ scripts/screen.js
@@ -1,26 +1,33 @@
 function buttonStartgame()
 {
-  var x = document.getElementsByClassName("loadingScreen");
-      for (var i = 0; i < x.length; i++)
-        {
-          x[i].style.display = "block";
-        }
+    var x = document.getElementsByClassName("loadingScreen");
+    for (var i = 0; i < x.length; i++)
+    {
+      x[i].style.display = "block";
+    }
+
+    var y = document.getElementById("initialMenuScreen");
+    y.style.display = "none";
 }
 function buttonLoadgame()
 {
-  //document.getElementById('a').style.display='block';
   var x = document.getElementsByClassName("loadingScreen2");
-      for (var i = 0; i < x.length; i++)
-        {
-          x[i].style.display = "block";
-        }
+  for (var i = 0; i < x.length; i++)
+  {
+    x[i].style.display = "block";
+  }
+
+  var y = document.getElementById("initialMenuScreen");
+  y.style.display = "none";
 }
 function buttonAchievement()
 {
-  //document.getElementById('a').style.display='block';
   var x = document.getElementsByClassName("achievementScreen");
-      for (var i = 0; i < x.length; i++)
-        {
-          x[i].style.display = "block";
-        }
+  for (var i = 0; i < x.length; i++)
+  {
+    x[i].style.display = "block";
+  }
+
+  var y = document.getElementById("initialMenuScreen");
+  y.style.display = "none";
 }
