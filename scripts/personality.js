6229808ef558821492188df6327b18478fa4345e
이형우 <32788447+tkddltz@users.noreply.github.com>
2017-12-01 01:03:25 +0900

    edit square class, personality.js

--- scripts/personality.js
+++ scripts/personality.js
@@ -1,55 +1,48 @@
-
-var prior=1;
+var prior=0;
+var stack=new Array();
 var row1Squares = document.querySelectorAll('.row1 .square');
-var latest=row1Squares[1];
 for(var i = 0; i < row1Squares.length; i++) {
-  var node = row1Squares[i];
-  node.onclick=function(){
-    if(this.classList.contains('enabled')&&this==latest){
-      this.classList.toggle('enabled');
-      prior--;
-    }
-    else if (!this.classList.contains('enabled')){
-      this.classList.toggle('enabled');
-      this.appendChild(document.createTextNode(prior));
-      latest=this;
-      if(prior!=6)
-        prior++;
-    }
-  }
+	var node = row1Squares[i];
+	node.onclick=function(){
+		if(!this.classList.contains('enabled')){
+			this.classList.add('enabled');
+			stack.push(this.textContent);
+			var text=document.createTextNode(stack[prior]);
+			document.getElementById("demo").appendChild(text);
+			prior++
+		}
+	}
 }
 var row2Squares = document.querySelectorAll('.row2 .square');
 for(var i = 0; i < row2Squares.length; i++) {
-  var node = row2Squares[i];
-  node.onclick=function(){
-    if(this.classList.contains('enabled')&&this==latest){
-      this.classList.toggle('enabled');
-      prior--;
-    }
-    else if (!this.classList.contains('enabled')){
-      this.classList.toggle('enabled');
-      this.appendChild(document.createTextNode(prior));
-      latest=this;
-      if(prior!=6)
-        prior++;
-    }
-  }
+	var node = row2Squares[i];
+	node.onclick=function(){
+		if(!this.classList.contains('enabled')){
+			this.classList.add('enabled');
+			stack.push(this.textContent);
+			var text=document.createTextNode(stack[prior]);
+			document.getElementById("demo").appendChild(text);
+			prior++;
+		}
+	}
 }
 function reset(){
-  prior=1;
-  for(var i = 0; i < row1Squares.length; i++) {
-    var node = row1Squares[i];
-    if(node.classList.contains('enabled')){
-      node.classList.toggle('enabled');
+	for(var i = 0; i < row1Squares.length; i++) {
+		var node = row1Squares[i];
+		if(node.classList.contains('enabled')){
+			node.classList.toggle('enabled');
+		}
+	}
+	for(var i = 0; i < row2Squares.length; i++) {
+		var node = row2Squares[i];
+		if(node.classList.contains('enabled')){
+			node.classList.toggle('enabled');
+		}
 
-    }
-  }
-  for(var i = 0; i < row2Squares.length; i++) {
-    var node = row2Squares[i];
-    if(node.classList.contains('enabled')){
-      node.classList.toggle('enabled');
-
-    }
-
-  }
+	}
+	for(var i=0;i<prior;i++){
+		stack.pop()
+	}
+	prior=0;
+	document.getElementById("demo").innerHTML="";
 }
