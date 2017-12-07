15700c9870aef829e3bc7dad0d38260c37d20f04
JeonHyeonseong <32788412+JeonHyeonseong@users.noreply.github.com>
2017-12-07 23:49:26 +0900

    Create ui_overall.js

---
+++ scripts/ui_overall.js
@@ -0,0 +1,14 @@
+let Department = {
+// DEPARTMENT   :   VALUE
+    MATH        :   0,      0   :   'MATH',
+    PHYS        :   1,      1   :   'PHYS',
+    CHEM        :   2,      2   :   'CHEM',
+    LIFE        :   3,      3   :   'LIFE',
+    MSE         :   4,      4   :   'MSE',
+    ME          :   5,      5   :   'ME',
+    IME         :   6,      6   :   'IME',
+    EE          :   7,      7   :   'EE',
+    CSE         :   8,      8   :   'CSE',
+    CE          :   9,      9   :   'CE',
+    CITE        :   10,     10  :   'CITE'
+}
