6f0b74d314400b24e37b2ff17ae9b2230000ab84
Yoonbeomjoon <yoonbj@postech.ac.kr>
2017-12-09 20:19:16 +0900

    process

--- scripts/process.js
+++ scripts/process.js
@@ -43,23 +43,10 @@ function animationLoop() {
 
 
 
-var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+var app = new PIXI.Application(1920, 1080, {backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
 
 
-
-// create a new Sprite from an image path
-var bunny = PIXI.Sprite.fromImage('images/ponix/character_application_01.png');
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
-// move the sprite to the center of the screen
-bunny.x = app.renderer.width *0.5;
-bunny.y = app.renderer.height *0.6;
-
-bunny.scale.x*=0.2;
-bunny.scale.y*=0.2;
-
-app.stage.addChild(bunny);
 
 
 var ruler = PIXI.Sprite.fromImage('images/ruler.png');
@@ -67,10 +54,15 @@ var ruler = PIXI.Sprite.fromImage('images/ruler.png');
 //ruler.anchor.set(0.5);
 // move the sprite to the center of the screen
 ruler.x = app.renderer.width *0.5;
-ruler.y = app.renderer.height *(-0.04);
+ruler.y = app.renderer.height *(-0.03);
+ruler.x-=400;
 
-//ruler.scale.x*=0.2;
-//ruler.scale.y*=0.2;
+ruler.scale.x*=1.24;
+ruler.scale.y*=1.24;
+// create a new Sprite from an image path
+
+
+
 
 app.stage.addChild(ruler);
 
@@ -81,26 +73,68 @@ app.stage.addChild(ruler);
  });
 
 
+var room = PIXI.Sprite.fromImage('room/room_default_day.png');
+room.y+=100;
+app.stage.addChild(room);
 
+var bunny = PIXI.Sprite.fromImage('room/room_default_ponix.png');
+// center the sprite's anchor point
+//bunny.anchor.set(0.5);
+// move the sprite to the center of the screen
+//bunny.x = app.renderer.width *0.5;
+//bunny.y = app.renderer.height *0.6;
 
+bunny.y+=100
+app.stage.addChild(bunny);
 
+var numberstyle = new PIXI.TextStyle({
+  fontFamily: 'Consolas',
+  fontSize: 80,
+  fill: ['#ffffff', '#00ff99'], // gradient
+  stroke: '#4a1850',
+  strokeThickness: 5,
+  dropShadow: true,
+  dropShadowColor: '#000000',
+  dropShadowBlur: 4,
+  dropShadowAngle: Math.PI / 6,
+  dropShadowDistance: 6,
+  wordWrap: true,
+  wordWrapWidth: 440
+  });
+var daynumber = 150;
+var dday;
 
+ 	var dday = new PIXI.Text(daynumber,numberstyle);
+ app.ticker.add(()=>
+ {
+ 	delta=100;
+ 	app.stage.removeChild(dday);
+ 	delete dday;
+ 	daynumber-=1;
+ 	var dday = new PIXI.Text(daynumber,numberstyle);
+  dday.anchor.set(0.5);
+  dday.x = 260;
+  dday.y = 480
+  app.stage.addChild(dday);
+
+ });
+   
+/*
 var graphics = new PIXI.Graphics();
   graphics.lineStyle(2, 0xFF00FF, 1);
   graphics.beginFill(0xFFFF99, 0.25);
   graphics.drawRect(50, 200, 700, 300);
-  app.stage.addChild(graphics);
-
+ */
 var leftblock = new PIXI.Graphics();
   //leftblock.lineStyle(2, 0xFF00FF, 1);
   leftblock.beginFill(0x1099bb, 1);
-  leftblock.drawRect(0, 0, 200, 100);
+  leftblock.drawRect(0, 0, 500, 100);
   app.stage.addChild(leftblock);
 
 var rightblock = new PIXI.Graphics();
   //rightblock.lineStyle(2, 0xFF00FF, 1);
   rightblock.beginFill(0x1099bb, 1);
-  rightblock.drawRect(600, 0, 200, 100);
+  rightblock.drawRect(1420, 0, 500, 100);
   app.stage.addChild(rightblock);
 
 
