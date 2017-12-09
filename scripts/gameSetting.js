1dda702d854c19df71d784f8f7abad58877e2a27
HyeonseongJeon <hyeonz0415@postech.ac.kr>
2017-12-09 20:28:21 +0900

    implement model

--- scripts/gameSetting.js
+++ scripts/gameSetting.js
@@ -1,3 +1,6 @@
+let healthPerLecture = -0.005;
+
+
 var thisGame = {};
 
 thisGame.department;
@@ -7,12 +10,28 @@ thisGame.timeTable;
 thisGame.status = {};
 thisGame.status.health = 1;
 thisGame.status.grade = 1;
+//  grade = (수행한 로드)/(전체 로드)
 thisGame.status.relationship = 1;
   //  이번 게임의 스탯
-thisGame.status.load;
+
+thisGame.load = 0;
   // 학기 전체의 load;
 
 var dayofweek = ["월", "화", "수", "목", "금", "토", "일"];
+
+let Load = {
+  "QUIZ" : 0.8,
+
+  "ASSIGN" : 1.2,
+  "LABASSIGN" : 0.4,
+  "HOMEWORK" : 0.8,
+
+  "MIDTERM" : 2.4,      //  두번 있음, 4.8
+  "FINAL" : 2.4,        //  두번 있음, 4.8
+  "3TEST" : 1.6,        //  두번 있음, 3.2
+
+  "PRESENTATION" : 1.6  //  두번 있음, 3.2
+}
 
 let Period = {
   // 시간 : 교시
@@ -37,7 +56,7 @@ class SingleEvent {
     _deltaHealth,         //  float
     _deltaRelationship,   //  float
     _isLecture,           //  boolean
-    _load,                //  float
+    //_load,                  2*16 array
     _lectureNumber        //  String
   ) {
     //  event
@@ -48,14 +67,193 @@ class SingleEvent {
 
     //  lecture
     this.isLecture = _isLecture;  //  lecture ? true : false
-    this.load = _load;  //  duration이랑 구분해야됨, 한 학기 전체 로드
+    this.load = Array(16);  //  duration이랑 구분해야됨, 한 학기 전체 로드
+    for(var i=0; i<16; i++) {
+      this.load[i] = Array(2);
+      this.load[i][0] = 0;
+      this.load[i][1] = "";
+    }
     this.lectureNumber = _lectureNumber;  //  학수번호
   }
 //  model 만드시는 분께서 capacity(남은 가용시간)를 받아 load를 업데이트 시키는 메소드도 만들어 주세요
+  addQuiz(weight) {
+    for(var i=0; i<16; i++) {
+      if( i===7 || i===15 ) {
+        continue;
+      }
+      this.load[i][0] += Load["QUIZ"]*weight;
+      this.load[i][1] += "퀴즈가 있습니다.<br/>";
+    }
+  }
+
+  addHomeWork2Week(weight) {
+    for(var i=0; i<16; i++) {
+      if( i===7 || i===15 ) {
+        continue;
+      }
+      if( i%2 === 0 ){
+        continue;
+      }
+      this.load[i][0] += Load["HOMEWORK"]*weight;
+      this.load[i][1] += "숙제가 있습니다.<br/>";
+    }
+  }
+  addHomeWork4Week(weight) {
+    // 4주에 한번씩 숙제가 나옴
+    // 2, 6, 10, 14주에 나오며 +-1주 adjust
+    var adjust = Math.floor(Math.random() * 3) - 1;
+
+    for(var i=0; i<4; i++) {
+      this.load[i*4+1+adjust][0] += Load["HOMEWORK"]*weight;
+      this.load[i*4+1+adjust][1] += "숙제가 있습니다.<br/>";
+    }
+  }
+
+  add3Assign(weight) {
+    // adjust as plus 1 or minus 1 or 0
+    var adjust = Math.floor(Math.random() * 3) - 1;
+
+    // assign 1
+    this.load[3+adjust][0] += Load["ASSIGN"]*weight;
+    this.load[4+adjust][0] += Load["ASSIGN"]*weight;
+    // assign 2
+    this.load[8+adjust][0] += Load["ASSIGN"]*weight;
+    this.load[9+adjust][0] += Load["ASSIGN"]*weight;
+    // assign 3
+    this.load[12+adjust][0] += Load["ASSIGN"]*weight;
+    this.load[13+adjust][0] += Load["ASSIGN"]*weight;
+
+    // assign 1
+    this.load[3+adjust][0] += "어사인먼트1이 나왔습니다.<br/>";
+    this.load[4+adjust][0] += "어사인먼트1 제출기한이 임박했습니다!<br/>";
+    // assign 2
+    this.load[8+adjust][0] += "어사인먼트2가 나왔습니다.<br/>";
+    this.load[9+adjust][0] += "어사인먼트2 제출기한이 임박했습니다!<br/>";
+    // assign 3
+    this.load[12+adjust][0] += "어사인먼트3가 나왔습니다.<br/>";
+    this.load[13+adjust][0] += "어사인먼트3 제출기한이 임박했습니다!<br/>";
+  }
+
+  addLabAssign(weight) {
+    for(var i=0; i<16; i++) {
+      if( i===7 || i===15 ) {
+        continue;
+      }
+      this.load[i][0] += Load["LABASSIGN"]*weight;
+      this.load[i][1] += "랩(TA수업/연습시간) 과제가 있습니다.<br/>";
+    }
+  }
+
+  addMidTerm(weight) {
+    this.load[6][0] += Load["MIDTERM"]*weight;
+    this.load[7][0] += Load["MIDTERM"]*weight;
+
+    this.load[6][1] += "중간고사에 대비하세요!<br/>";
+    this.load[7][1] += "중간고사가 있습니다!<br/>";
+  }
+
+  addFinal(weight) {
+    this.load[14][0] += Load["MIDTERM"]*weight;
+    this.load[15][0] += Load["MIDTERM"]*weight;
+
+    this.load[14][1] += "기말고사에 대비하세요!<br/>";
+    this.load[15][1] += "기말고사가 있습니다!<br/>";
+  }
+
+  add3Test(weight) {
+    this.load[4][0] += Load["3TEST"]*weight;
+    this.load[5][0] += Load["3TEST"]*weight;
+    this.load[9][0] += Load["3TEST"]*weight;
+    this.load[10][0] += Load["3TEST"]*weight;
+    this.load[14][0] += Load["3TEST"]*weight;
+    this.load[15][0] += Load["3TEST"]*weight;
+
+    this.load[4][1] += "첫번째 중간고사에 대비하세요!<br/>";
+    this.load[5][1] += "첫번째 중간고사가 있습니다.<br/>";
+    this.load[9][1] += "두번째 중간고사에 대비하세요!<br/>";
+    this.load[10][1] += "두번째 중간고사가 있습니다.<br/>";
+    this.load[14][1] += "기말고사에 대비하세요!<br/>";
+    this.load[15][1] += "기말고사가 있습니다!<br/>";
+  }
+
+  addMidPresentation(weight, description) {
+    var adjust = Math.floor(Math.random() * 3) - 1;
+    this.load[4+adjust][0] += Load["PRESENTATION"]*weight;
+    this.load[5+adjust][0] += Load["PRESENTATION"]*weight;
+    this.load[4+adjust][1] += "중간발표를 준비하세요!"+description+"<br/>";
+    this.load[5+adjust][1] += "중간발표가 있습니다."+description+"<br/>";
+  }
+
+  addFinalPresentation(weight, description) {
+    var adjust = Math.floor(Math.random() * 3) - 1;
+    this.load[12+adjust][0] += Load["PRESENTATION"]*weight*2;
+    this.load[13+adjust][0] += Load["PRESENTATION"]*weight*2;
+    this.load[12+adjust][1] += "기말발표를 준비하세요!"+description+"<br/>";
+    this.load[13+adjust][1] += "기말발표가 있습니다."+description+"<br/>";
+  }
+
+  addLoad(load, description, period) {
+    this.load[period][0] += load;
+    this.load[period][1] += description;
+  }
+
 }
 
 //  SingleEvent pool
-let Event = {
-  "객체지향프로그래밍A" : new SingleEvent("객체지향프로그래밍", dayofweek[0], Period["AM 11:00"], 90, -0.02, 0, true, 100, "CSED232"),
-  "객체지향프로그래밍B" : new SingleEvent("객체지향프로그래밍", dayofweek[2], Period["AM 11:00"], 90, -0.02, 0, true, 100, "CSED232")
-} // 전체 과목(Period 단위)를 저장
+var Event = {};
+
+
+// MATH
+Event["현대대수학I"] = new SingleEvent("현대대수학I", 1.5, healthPerLecture, 0, true, "MATH301");  //5, 112시간
+Event["현대대수학I"].addQuiz(5);
+Event["현대대수학I"].addMidTerm(5);
+Event["현대대수학I"].addFinal(5);
+
+Event["해석학I"] = new SingleEvent("해석학I", 1.5, healthPerLecture, 0, true, "MATH311");  //5, 112시간
+Event["해석학I"].addQuiz(5);
+Event["해석학I"].addMidTerm(5);
+Event["해석학I"].addFinal(5);
+
+
+Event["미분방정식"] = new SingleEvent("미분방정식", 1.5, healthPerLecture, 0, true, "MATH200");  // 1, 22.4시간
+Event["미분방정식"].addQuiz(1);
+Event["미분방정식"].addMidTerm(1);
+Event["미분방정식"].addFinal(1);
+
+Event["확률및통계"] = new SingleEvent("확률및통계", 1.5, healthPerLecture, 0, true, "MATH230");  // 2, 44.8시간
+Event["확률및통계"].addQuiz(2);
+Event["확률및통계"].addMidTerm(2);
+Event["확률및통계"].addFinal(2);
+
+Event["이산수학"] = new SingleEvent("이산수학", 1.5, healthPerLecture, 0, true, "MATH261");  // 1, 22.4시간
+Event["이산수학"].addQuiz(1);
+Event["이산수학"].addMidTerm(1);
+Event["이산수학"].addFinal(1);
+
+
+// IME
+Event["최적화개론"] = new SingleEvent("최적화개론", 1.5, healthPerLecture, 0, true, "IMEN261"); // 1
+Event["최적화개론"].add3Test(1);
+Event["최적화개론"].addHomeWork4Week(1);
+
+Event["정보시스템기술"] = new SingleEvent("정보시스템기술", 1.5, healthPerLecture, 0, true, "IMEN281");  // 6, 134.4시간
+Event["정보시스템기술"].addLabAssign(6);
+Event["정보시스템기술"].add3Assign(6);
+Event["정보시스템기술"].addMidTerm(6);
+Event["정보시스템기술"].addFinal(6);
+
+Event["제품생산공정설계I"] = new SingleEvent("제품생산공정설계I", 1, healthPerLecture*(2/3), 0, true, "IMEN230");  // 1, 14.4시간
+Event["제품생산공정설계I"].addMidTerm(1);
+Event["제품생산공정설계I"].addFinal(1);
+Event["제품생산공정설계I"].addMidPresentation(1, "(동영상)");
+Event["제품생산공정설계I"].addFinalPresentation(1, "(동영상)");
+
+Event["실험계획개론"] = new SingleEvent("실험계획개론", 1.5, healthPerLecture, 0, true, "IMEN242"); // 5
+Event["실험계획개론"].add3Test(5);
+Event["실험계획개론"].addHomeWork2Week(5);
+
+Event["비즈니스플래닝"] = new SingleEvent("비즈니스플래닝", 1.5, healthPerLecture, 0, true, "IMEN412"); // 4
+Event["비즈니스플래닝"].addMidTerm(4);
+Event["비즈니스플래닝"].addFinal(4);
+Event["비즈니스플래닝"].addMidPresentation(4, "(사업계획서)");
+Event["비즈니스플래닝"].addFinalPresentation(4, "(사업계획데모, 전문투자자)");
--- scripts/model.js
+++ scripts/model.js
@@ -1,52 +1,172 @@
-function model() { // 일단은 1주 진행
-  var capacity;
+var thisDuration = { };
+  //  얘네는 성향테스트에 따라서 조정되어야 함
+thisDuration.meal = 2;
+thisDuration.dinguldingul = 4;
 
+  //  얘는 랜덤하게 매일매일 성향에따라 정해지는걸로
+thisDuration.hobby = 4;
+
+// 미구현 (동아리)
+thisDuration.circle = 0;
+
+thisGame.department = Department["MATH"];
+thisGame.timeTable = new Array(7);
+for( var i=0; i<7; i++ ) {
+  thisGame.timeTable[i] = new Array(11);
+}
+thisGame.timeTable[0][2] = Event["현대대수학I"];
+thisGame.timeTable[2][2] = Event["현대대수학I"];
+
+thisGame.timeTable[0][3] = Event["확률및통계"];
+thisGame.timeTable[2][3] = Event["확률및통계"];
+
+thisGame.timeTable[1][1] = Event["이산수학"];
+thisGame.timeTable[3][1] = Event["이산수학"];
+
+thisGame.timeTable[1][3] = Event["해석학I"];
+thisGame.timeTable[3][3] = Event["해석학I"];
+
+thisGame.timeTable[1][5] = Event["미분방정식"];
+thisGame.timeTable[3][5] = Event["미분방정식"];
+
+var lectureList = new Array(5);
+lectureList[0] = Event["현대대수학I"];
+lectureList[1] = Event["확률및통계"];
+lectureList[2] = Event["이산수학"];
+lectureList[3] = Event["해석학I"];
+lectureList[4] = Event["미분방정식"];
+
+function calculateLoad(lecture) {
+  for(var i=0; i<16; i++)
+    thisGame.load += lecture.load[i][0];
+}
+
+calculateLoad(Event["현대대수학I"]);
+calculateLoad(Event["확률및통계"]);
+calculateLoad(Event["이산수학"]);
+calculateLoad(Event["해석학I"]);
+calculateLoad(Event["미분방정식"]);
+
+model();
+
+function model() { // 일단은 1주 진행
+  var capacity=0;
+
+  var completedLoad = 0;
+  //  총 수행한 로드 / grade = (총 수행한 로드)/(전체 로드)
   for (let week =0; week < 16; week++){
     //7 * 11 시간표 배열이 넘어온다고 가정 (7일 10교시 + 10시 이후)
     // 16주 동안 진행하고, 16개의 이벤트 발생 -> critical 이벤트?
+    var capacityPerWeek = 0;
+    //  얘는 한 주 동안 남은 시간을 계산하여 과제, 공부, 체력충전으로 전환시킴
 
     for (let day = 0; day < 7; day++){
       var firstPeriod = -1;
 
       for (let peri = 0; peri < 11; peri++){
-        if(thisGame.timetable[i][j] != "" && firstPeriod == -1)
+        if( thisGame.timeTable[day][peri] !== undefined &&
+          thisGame.timeTable[day][peri] !== "" && firstPeriod == -1)
           firstPeriod = peri;
         } // 첫 수업시간 찾음
 
-        capacityUpdate(capacity, firstPeriod);
+        capacity = capacityUpdate(firstPeriod);
+        //console.log("오늘의 가용시간: "+capacity+"\n");
 
         for (let peri = firstPeriod; peri < 11; peri++){
-          if (thisGame.timeTable[i][j] == "") continue;
-          let nowEvent = Event[thisGame.timeTable[i][j]];
+          if (thisGame.timeTable[day][peri] === undefined ||
+            thisGame.timeTable[day][peri] === "") continue;
+          let nowEvent = thisGame.timeTable[day][peri];
 
-          thisGame.status.health += statUpdate(timeDependency(nowEvent));
+          thisGame.status.health += statUpdate(nowEvent.deltaHealth*timeDependency(peri));
           thisGame.status.relationship += statUpdate(nowEvent.deltaRelationship);
           capacity -= nowEvent.duration;
           // 스텟 업데이트, 가용시간 - 소모시간
         }
+        //console.log("오늘의 남은시간: "+capacity+"\n");
+        if( capacity <= 0 ) {
+          capacity = 0;
+        }
+        // 가용시간은 0 이하로 내려갈 수 없음
+
+        capacityPerWeek += capacity;
+        // 남은 시간은 주간 가용시간으로 넘김
       }
+      completedLoad = weekCapacityUpdate(capacityPerWeek, completedLoad, week);
+      // 주간 로드 반영
+
+      console.log(week+"주차 / 현재 체력: "+thisGame.status.health+"\n");
+      console.log(week+"주차 / 남은 가용시간: "+capacityPerWeek+"\n");
+      console.log("총 수행한 로드(누적): "+completedLoad+"\n");
       eventHandler();
       // 이벤트 발생 -> 랜덤 발생하는 알고리즘 필요
     }
+    console.log("이번 학기의 총 로드: "+thisGame.load);
+    console.log("Grade: "+completedLoad/thisGame.load);
 }
 function eventHandler(){
 
 } // 이벤트 발생 (UI랑 연결?)
-function capacityUpdate(capacity, firstPeriod){
-  var wakeUpTime = 8 + 90 * firstPeriod;
+function capacityUpdate(firstPeriod){
+  var wakeUpTime = 8 + 1.5 * firstPeriod;
+  var tempCapacity = 24;
+  tempCapacity -= wakeUpTime;
+  // 가용시간을 24시 - 오전 첫 수업 시간로 업데이트
 
-  capacity = 24 - wakeUpTime;
-} // 가용시간을 24시 - 오전 첫 수업 시간로 업데이트
+  tempCapacity -= thisDuration.meal;
+  tempCapacity -= thisDuration.dinguldingul;
+  // 밥먹는 시간, 뒹굴거리는 시간 뻄
+
+  tempCapacity -= thisDuration.circle;
+  tempCapacity -= thisDuration.hobby;
+  //  동아리, 취미 시간 뺌
+
+  return tempCapacity;
+}
 
 function statUpdate(deltastat) {
   return 1 * deltastat;
 } // stat을 조절하는 function
 
-function timeDependency(nowEvent) {
-  switch(nowEvent.period){
-    case Period["AM 8:00"] : return nowEvent.deltaHealth * 1.3
-    case Period["AM 9:30"] : return nowEvent.deltaHealth * 1.2
-    case Period["AM 11:00"] : return nowEvent.deltaHealth * 1.1
+function timeDependency(period) {
+  switch(period){
+    case Period["AM 8:00"] : return 1.3;
+    case Period["AM 9:30"] : return 1.2;
+    case Period["AM 11:00"] : return 1.1;
+    default : return 1;
   }
   return nowEvent.deltaHealth
 } // 시간에 따른 체력 변화량 조절 (오전 수업 체력 소모량 가중치 있음)
+
+function weekCapacityUpdate(capacityPerWeek, completedLoad, week) {
+  var tempLoadPerWeek = 0;
+  for(var i=0; i<lectureList.length; i++) {
+    tempLoadPerWeek += lectureList[i].load[week][0];
+  }
+
+  if( capacityPerWeek > tempLoadPerWeek ) {
+    // 남은 주간 가용시간이 해야할 일보다 많을 때
+    completedLoad += tempLoadPerWeek;
+    capacityPerWeek -= tempLoadPerWeek;
+
+    // 수행한 로드의 0.005배가 체력에서 깎임
+    thisGame.status.health -= tempLoadPerWeek*0.005;
+
+    // 남은 시간의 0.01배가 체력으로 들어감
+    thisGame.status.health += capacityPerWeek*0.01;
+
+    // 체력은 1보다 클 수 없음
+    if( thisGame.status.health >= 1 )
+      thisGame.status.health = 1;
+  }
+  else {
+    // 남은 주간 가용시간이 해야할 일보다 적을 때
+    completedLoad += capacityPerWeek;
+
+    // 수행한 로드의 0.005배가 체력에서 깎임
+    thisGame.status.health -= capacityPerWeek*0.005;
+  }
+  // capacityPerWeek을 과제, 공부, 체력충전으로 전환
+
+  console.log(week+"주차 / 수행해야 할 로드: "+tempLoadPerWeek+"\n");
+  return completedLoad;
+}
